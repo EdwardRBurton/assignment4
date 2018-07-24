3192e922c773305d1c5b47ca2142efe21339be69
Ed Burton <erburton2@gmail.com>
2018-07-24 14:21:16 -0500

    Added Validation for assignment 5

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,8 @@
 class Artist < ApplicationRecord
   has_and_belongs_to_many :albums
+
+  # Validations
+  validates :name, length: { minimum: 3, maximum: 255 }, presence: true
+  validates :name, uniqueness: true
+  validates :started, numericality:{greater_than_or_equal_to: 1}, presence: true
 end
--- app/models/label.rb
+++ app/models/label.rb
@@ -1,3 +1,9 @@
 class Label < ApplicationRecord
   has_many :albums
+
+  # Validations
+  validates :name, length: {minimum: 5, maximum: 50}, presence: true
+  validates :name, uniqueness: true
+  validates :ceo, length: {minimum: 5, maximum: 50}, presence: true
+  validates :founded, numericality:{greater_than_or_equal_to: 1}, presence: true
 end
