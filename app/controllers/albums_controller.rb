74987a21321289fb4251b38f0b6e3f69e0d2d2d2
Ed Burton <erburton2@gmail.com>
2018-07-24 15:51:23 -0500

    Trying to get album names to display on artists

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -71,6 +71,6 @@ class AlbumsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def album_params
-      params.require(:album).permit(:title, :genre, :year, :plays, :label_id, :artist_ids =>[])
+      params.require(:album).permit(:title, :genre, :year, :plays, :label_id, :artist_id =>[])
     end
 end
