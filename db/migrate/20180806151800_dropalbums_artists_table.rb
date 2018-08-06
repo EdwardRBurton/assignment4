941b29f29b1c0aa7995d75af08a39cbff5f8ae5b
Ed Burton <erburton2@gmail.com>
2018-08-06 13:30:37 -0500

    Removed old joiner, added new one

--- db/migrate/20180806151800_dropalbums_artists_table.rb
+++ db/migrate/20180806151800_dropalbums_artists_table.rb
@@ -1,5 +1,5 @@
 class DropalbumsArtistsTable < ActiveRecord::Migration[5.2]
   def change
-    drop_table :table_name
+    drop_table :albums_artists
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_16_173956) do
+ActiveRecord::Schema.define(version: 2018_08_06_155206) do
 
   create_table "albums", force: :cascade do |t|
     t.string "title"
@@ -23,13 +23,11 @@ ActiveRecord::Schema.define(version: 2018_07_16_173956) do
     t.index ["label_id"], name: "index_albums_on_label_id"
   end
 
-  create_table "albums_artists", force: :cascade do |t|
-    t.integer "album_id"
-    t.integer "artist_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["album_id"], name: "index_albums_artists_on_album_id"
-    t.index ["artist_id"], name: "index_albums_artists_on_artist_id"
+  create_table "albums_artists", id: false, force: :cascade do |t|
+    t.integer "album_id", null: false
+    t.integer "artist_id", null: false
+    t.index ["album_id", "artist_id"], name: "index_albums_artists_on_album_id_and_artist_id"
+    t.index ["artist_id", "album_id"], name: "index_albums_artists_on_artist_id_and_album_id"
   end
 
   create_table "artists", force: :cascade do |t|
