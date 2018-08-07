54ffcdbcbd0d0a58307b7f59763411cd6f8eac9b
Ed Burton <erburton2@gmail.com>
2018-08-07 12:09:32 -0500

    Assignment 6 completed: added devise rails search

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,9 +3,9 @@ class Album < ApplicationRecord
   has_and_belongs_to_many :artists
 
   validates :label_id, presence: true
-  validates :artist_ids, presence: true
   validates :year, presence: true, length: {maximum: 4, minimum: 4}
   validates :title, presence: true, length: {maximum: 250, minimum: 3}
   validates :genre, presence: true, length: {maximum: 250, minimum: 3}
   validates :plays, presence: true, length: {maximum: 10, minimum: 1}
+  validates :artist_ids, presence: true
 end
